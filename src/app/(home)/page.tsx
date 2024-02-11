18ac8176d3bc59097c1bd9a41309651aa6c4f9d9
Fernando Valença <fernandovalenca2013@gmail.com>
2024-02-11 14:05:25 -0300

    feat: add welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,21 @@
 import Header from '@/components/shared/header';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 export default function Home() {
+  const currentDate = format(new Date(), "EEEE',' d 'de' MMMM", {
+    locale: ptBR,
+  });
   return (
     <main>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h1 className="text-xl font-bold">Olá, Fernando</h1>
+        <p className="text-sm">
+          {currentDate.charAt(0).toUpperCase() + currentDate.slice(1)}
+        </p>
+      </div>
     </main>
   );
 }
