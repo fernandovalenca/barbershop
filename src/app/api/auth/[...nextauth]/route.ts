61cb248929ff739f376499fee82e547c69b2cef7
Fernando Valença <fernandovalenca2013@gmail.com>
2024-02-12 17:54:41 -0300

    fix: authentication flow in next auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,17 @@
 import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { db } from '@/lib/prisma';
 import { Adapter } from 'next-auth/adapters';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import GoogleProvider from 'next-auth/providers/google';
+import { db } from '@/lib/prisma';
 
-export const authOptions = {
+export const handler = NextAuth({
   adapter: PrismaAdapter(db) as Adapter,
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-};
+});
 
-export default NextAuth(authOptions);
+export { handler as GET, handler as POST };
