90eb9c393c291e17e57d9c193c74ce3662ad3f86
Fernando Valença <fernandovalenca2013@gmail.com>
2024-02-14 17:43:14 -0300

    refactor: ensure hours generated for scheduling are in accordance with the date and time of the day

--- src/helpers/generateDayTimeList.ts
+++ src/helpers/generateDayTimeList.ts
@@ -1,6 +1,18 @@
-import { setHours, setMinutes, format, addMinutes } from 'date-fns';
+import {
+  setHours,
+  setMinutes,
+  format,
+  addMinutes,
+  startOfDay,
+  isAfter,
+  isBefore,
+} from 'date-fns';
 
 export function generateDayTimeList(date: Date): string[] {
+  const currentDate = new Date();
+  
+  if (isBefore(startOfDay(date), startOfDay(currentDate))) return [];
+
   const startTime = setMinutes(setHours(date, 9), 0); // Set start time to 09:00
   const endTime = setMinutes(setHours(date, 21), 0); // Set end time to 21:00
   const interval = 45; // interval in minutes
@@ -9,7 +21,9 @@ export function generateDayTimeList(date: Date): string[] {
   let currentTime = startTime;
 
   while (currentTime <= endTime) {
-    timeList.push(format(currentTime, 'HH:mm'));
+    if (isAfter(currentTime, currentDate)) {
+      timeList.push(format(currentTime, 'HH:mm'));
+    }
     currentTime = addMinutes(currentTime, interval);
   }
 
