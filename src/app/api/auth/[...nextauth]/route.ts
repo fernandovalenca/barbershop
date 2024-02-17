0c017dba08bbc86a1469353c2bb1e843a874b13c
Fernando Valença <fernandovalenca2013@gmail.com>
2024-02-17 14:03:20 -0300

    fix: ensure auth options are exported from _lib folder

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,5 @@
-import NextAuth, { AuthOptions } from 'next-auth';
-import { Adapter } from 'next-auth/adapters';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import GoogleProvider from 'next-auth/providers/google';
-import { db } from '@/lib/prisma';
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      session.user = { ...session.user, id: user.id } as {
-        id: string;
-        name: string;
-        email: string;
-        image: string;
-      };
-
-      return session;
-    },
-  },
-  secret: process.env.NEXT_AUTH_SECRET as string,
-};
+import { authOptions } from '@/lib/authOptions';
+import NextAuth from 'next-auth/next';
 
 const handler = NextAuth(authOptions);
 
